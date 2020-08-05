e3c57c5d5c2ed78e3443473fc25591ed9ca09495
Nelson-E-S <nelson.e.sanabria1@gmail.com>
2020-08-05 12:18:33 -0400

    created header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () =>{
+    return (
+        <header>
+            <h4>THE NAUTILUS SPOTLIGHT</h4>
+            <h1>WHY THE LAWS OF PHYSICS ARE INEVITABLE</h1>
+            <span>READ NOW</span>
+        </header>
+    );
+}
+
+export default Header;
