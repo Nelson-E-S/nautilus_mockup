d556233077115e752c5cdefded10579f63b2ab29
Nelson-E-S <nelson.e.sanabria1@gmail.com>
2020-08-05 14:13:06 -0400

    import every created section to app js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SectionHeader from './components/SectionHeader';
+import SectionOne from './components/SectionOne';
+import SectionTwo from './components/SectionTwo';
+import SectionThree from './components/SectionThree';
+import SectionFour from './components/SectionFour';
+import SectionFive from './components/SectionFive';
+import SectionSix from './components/SectionSix';
+import SectionFooter from './components/SectionFooter';
 
 function App() {
   return (
     <div className="App">
-      
+      <SectionHeader />
+      <SectionOne />
+      <SectionTwo />
+      <SectionThree />
+      <SectionFour />
+      <SectionFive />
+      <SectionSix />
+      <SectionFooter />
     </div>
   );
 }
