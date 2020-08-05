0fe09de33c253a6c1c869d50eb0568d3d95a1d86
Nelson-E-S <nelson.e.sanabria1@gmail.com>
2020-08-05 12:54:26 -0400

    created sectiontwo component

---
+++ src/components/SectionTwo.js
@@ -0,0 +1,101 @@
+import React from 'react';
+
+const SectionTwo = () => {
+    return(
+        <section id="s2">
+            <div className="title">
+                <h2><em>Channels</em></h2>
+                <p>TUNE INT TO <em>NAUTILUS'</em> DEEP DIVE PORTALS</p>
+            </div>
+            <div className="set">
+                <div className="box">
+                    <h4>BIOLOGY+BEYOND</h4>
+                    <img src="#" alt="#" />
+                    <p>Making Sendse of the Genome, at Last</p>
+                    <em>by Adam Piore</em>
+                </div>
+                <div className="box">
+                    <h4>BIOLOGY+BEYOND</h4>
+                    <img src="#" alt="#" />
+                    <p>Making Sendse of the Genome, at Last</p>
+                    <em>by Adam Piore</em>
+                </div>
+                <div className="box">
+                    <h4>BIOLOGY+BEYOND</h4>
+                    <img src="#" alt="#" />
+                    <p>Making Sendse of the Genome, at Last</p>
+                    <em>by Adam Piore</em>
+                </div>
+                <div className="box">
+                    <h4>BIOLOGY+BEYOND</h4>
+                    <img src="#" alt="#" />
+                    <p>Making Sendse of the Genome, at Last</p>
+                    <em>by Adam Piore</em>
+                </div>
+                <div className="box">
+                    <h4>BIOLOGY+BEYOND</h4>
+                    <img src="#" alt="#" />
+                    <p>Making Sendse of the Genome, at Last</p>
+                    <em>by Adam Piore</em>
+                </div>
+                <div className="box">
+                    <h4>BIOLOGY+BEYOND</h4>
+                    <img src="#" alt="#" />
+                    <p>Making Sendse of the Genome, at Last</p>
+                    <em>by Adam Piore</em>
+                </div>
+                <div className="box">
+                    <h4>BIOLOGY+BEYOND</h4>
+                    <img src="#" alt="#" />
+                    <p>Making Sendse of the Genome, at Last</p>
+                    <em>by Adam Piore</em>
+                </div>
+                <div className="box">
+                    <h4>BIOLOGY+BEYOND</h4>
+                    <img src="#" alt="#" />
+                    <p>Making Sendse of the Genome, at Last</p>
+                    <em>by Adam Piore</em>
+                </div>
+            </div>
+            <div className="set">
+                <div className="box">
+                    <h4>BIOLOGY+BEYOND</h4>
+                    <img src="#" alt="#" />
+                    <p>Making Sendse of the Genome, at Last</p>
+                    <em>by Adam Piore</em>
+                </div>
+                <div className="box">
+                    <h4>BIOLOGY+BEYOND</h4>
+                    <img src="#" alt="#" />
+                    <p>Making Sendse of the Genome, at Last</p>
+                    <em>by Adam Piore</em>
+                </div>
+                <div className="box">
+                    <h4>BIOLOGY+BEYOND</h4>
+                    <img src="#" alt="#" />
+                    <p>Making Sendse of the Genome, at Last</p>
+                    <em>by Adam Piore</em>
+                </div>
+                <div className="box">
+                    <h4>BIOLOGY+BEYOND</h4>
+                    <img src="#" alt="#" />
+                    <p>Making Sendse of the Genome, at Last</p>
+                    <em>by Adam Piore</em>
+                </div>
+                <div className="box">
+                    <h4>BIOLOGY+BEYOND</h4>
+                    <img src="#" alt="#" />
+                    <p>Making Sendse of the Genome, at Last</p>
+                    <em>by Adam Piore</em>
+                </div>
+                <div className="box">
+                    <h4>BIOLOGY+BEYOND</h4>
+                    <img src="#" alt="#" />
+                    <p>Making Sendse of the Genome, at Last</p>
+                    <em>by Adam Piore</em>
+                </div>
+            </div>
+        </section>
+    );
+}
+export default SectionTwo;
