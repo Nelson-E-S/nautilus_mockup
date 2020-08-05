4c1d8defb432bfa1901aed02eced8a9cf98bf855
Nelson-E-S <nelson.e.sanabria1@gmail.com>
2020-08-05 13:57:14 -0400

    created sectionsix component

---
+++ src/components/SectionSix.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const SectionSix = () => {
+    return(
+        <section id="s6">
+            <div className="title">
+                <h2><em>Nautilus Prime</em></h2>
+                    <p>GET THE FULL DIGITAL EXPERIENCE</p>
+                    <span>MEMBERS SIGN IN</span>
+            </div>
+            <div className="content">
+                <img src="#" alt="#" />
+                <div>
+                    <p>Like <em>Nautilus</em>? You'll love <em>Nautilus</em> Prime.</p>
+                    <p><span>Unlimited online reading</span> Read it all, whenever, wherever, and however you like.</p>
+                    <p><span>Ad free</span> Get a pristine, ad-free reading experience.</p>
+                    <p><span>Tablet editions</span> All the content and design fo our award-winning print magazine, in PDF format-perfect for reading on you rtablet or desktop.</p>
+                    <p><span>eBook editions</span> We've made each monthly online issu e into an eBook, compatible with most e-readers.</p>
+                    <p><span>Event access</span> Prime members get priority to access to Nautilus events.</p>
+                    <div>
+                        <span class="left">Learn More</span>
+                        <span class="right">Join Prime</span>
+                    </div>
+                    <p>Current print susbscriber?<br />You're already a Prime member.</p>
+                </div>
+            </div>
+        </section>
+    );
+}
+export default SectionSix;
