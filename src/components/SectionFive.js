0a9e2b883c652f73e19dd522f05a137ce8c288c3
Nelson-E-S <nelson.e.sanabria1@gmail.com>
2020-08-05 13:47:09 -0400

    created sectionfive component

---
+++ src/components/SectionFive.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const SectionFive = () => {
+    return(
+        <section id="s5">
+            <div className="title">
+                <h2><em>Listen To Nautilus</em></h2>
+                    <p>NARRATED VERSION OF OUR STORIES</p>
+                    <span>ALL AUDIO ARTICLES</span>
+            </div>
+            <div class="content">
+                <div class="box">
+                    <img src="#" alt="#" />
+                    <h6>BIOLOGY</h6>
+                    <p>Turning Back the Clock on Human Evolution</p>
+                    <em>By Amy Maxmen</em>
+                </div>
+                <div class="box">
+                    <img src="#" alt="#" />
+                    <h6>BIOLOGY</h6>
+                    <p>Turning Back the Clock on Human Evolution</p>
+                    <em>By Amy Maxmen</em>
+                </div>
+                <div class="box">
+                    <img src="#" alt="#" />
+                    <h6>BIOLOGY</h6>
+                    <p>Turning Back the Clock on Human Evolution</p>
+                    <em>By Amy Maxmen</em>
+                </div>
+            </div>
+        </section>
+    );
+}
+export default SectionFive;
