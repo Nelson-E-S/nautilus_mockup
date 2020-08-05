5ea3b7b4f254db85ea98f2c77337c63fc4f7f70d
Nelson-E-S <nelson.e.sanabria1@gmail.com>
2020-08-05 17:19:23 -0400

    navbar styling completed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,15 +4,17 @@ import logo from '../assets/global/nautilus-logo-sprite@2x.png'
 const NavBar = () =>{
     return(
         <nav>
-            <img src={logo} alt="Nautilus logo" />
-            <ul>
-                <li>ISSUES</li>
-                <li>TOPIC</li>
-                <li>BLOG</li>
-                <li>NEWSLETTER</li>
-                <li>LOGIN</li>
-                <li className="sub">Subscribe</li>
-            </ul>
+            <div className="contentSpace">
+                <img src={logo} alt="Nautilus logo" />
+                <ul>
+                    <li>ISSUES</li>
+                    <li>TOPIC</li>
+                    <li>BLOG</li>
+                    <li>NEWSLETTER</li>
+                    <li>LOGIN</li>
+                    <li className="sub">SUBSCRIBE</li>
+                </ul>
+            </div>
         </nav>
     );
 }
