1124a3edf5101903482947b41e28ed896b39c05b
Nelson-E-S <nelson.e.sanabria1@gmail.com>
2020-08-05 13:11:00 -0400

    created sectionthree component

---
+++ src/components/SectionThree.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+const SectionThree = () => {
+    return(
+        <section id="s3">
+            <div className="title">
+                <h2><em>Popular On Nautilus</em></h2>
+                <div>
+                    <span className="left">Most Read</span>
+                    <span className="right">Most Shared</span>
+                </div>
+            </div>
+            <div className="reel">
+                <div className="a1">
+                    <img src="#" alt="#" />
+                    <h3>1 Does Theranos Mark the Peak of the Silicon Valley Bubble?</h3>
+                    <em>John Carreyrou talks to Nautilus about the lesson fo a $1 billon fund.</em>
+                    <span>BY MICHAEL SEGAL</span>
+                </div>
+                <div className="a2">
+                    <img src="#" alt="#" />
+                    <h3>2 Does Theranos Mark the Peak of the Silicon Valley Bubble?</h3>
+                    <em>John Carreyrou talks to Nautilus about the lesson fo a $1 billon fund.</em>
+                    <span>BY MICHAEL SEGAL</span>
+                </div>
+                <div className="a3">
+                    <h4>3 Does Theranos Mark the Peak of the Silicon Valley Bubble?</h4>
+                    <em>John Carreyrou talks to Nautilus about the lesson fo a $1 billon fund.</em>
+                    <span>BY MICHAEL SEGAL</span>
+                </div>
+                <div className="a4">
+                    <h4>4 Does Theranos Mark the Peak of the Silicon Valley Bubble?</h4>
+                    <em>John Carreyrou talks to Nautilus about the lesson fo a $1 billon fund.</em>
+                    <span>BY MICHAEL SEGAL</span>
+                </div>
+                <div className="a5">
+                    <h4>5 Does Theranos Mark the Peak of the Silicon Valley Bubble?</h4>
+                    <em>John Carreyrou talks to Nautilus about the lesson fo a $1 billon fund.</em>
+                    <span>BY MICHAEL SEGAL</span>
+                </div>
+                <div className="a6">
+                    <h4>6 Does Theranos Mark the Peak of the Silicon Valley Bubble?</h4>
+                    <em>John Carreyrou talks to Nautilus about the lesson fo a $1 billon fund.</em>
+                    <span>BY MICHAEL SEGAL</span>
+                </div>
+            </div>
+        </section>
+    );
+}
+export default SectionThree;
